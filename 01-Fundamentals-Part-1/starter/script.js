e7453c721c419a8ee702b5aeca75dac08739a7ab
Lbirk <lucasbirkenstock@protonmail.com>
2024-05-16 14:15:26 -0700

    dofijdsoifj

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,5 @@
 // alert("Hello asldfkjdslkfjdlksajflksadjfl")
 let person = { example: "hello", example2: "hallo", example3 : "hej"};
 alert(person.example);
+alert(person.example2);
+alert(person.example3);
