74791ad3e3294efc410883c064d01cbf7452342b
Lbirk <68677326+lucasbirkenstock@users.noreply.github.com>
2024-05-16 14:10:34 -0700

    asdf

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,1 +1,3 @@
-alert("Hello asldfkjdslkfjdlksajflksadjfl")
+// alert("Hello asldfkjdslkfjdlksajflksadjfl")
+let person = { example: "hello", example2: "hallo", example3 : "hej"}
+alert(person.example)
